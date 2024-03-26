c3f3db7cc6f7f1448be921b6cddb8807f4c6e6d4
Nigini Oliveira <nigini@gmail.com>
2024-03-26 16:43:41 -0700

    Added a Handlebars template to better handle the HTML content. Also, beautified the generated page with basic Bootstrap + added the article's metadata to the processed page.

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const config = {
     DOC_URL: "this",
+    SERVER_PORT: 7777
 }
 
+const fs = require('node:fs');
 const express = require('express');
 const {JSDOM} = require("jsdom");
 const {Readability} = require("@mozilla/readability");
+const Handlebars = require("handlebars");
 const app = express();
-const port = 7777;
 
-function response_page(parsed_article){
-    return `
-    <!DOCTYPE html>
-    <h1>${parsed_article.title}</h1>
-    <hr>
-    ${parsed_article.content}
-    `
+let article_template = "";
+
+fs.readFile("./templates/article.html", 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        article_template = data;
+    }
+});
+
+function response_page(parsed_article, source_url){
+    let page_template = Handlebars.compile(article_template);
+    return page_template({
+        title: parsed_article.title,
+        author: parsed_article.byline,
+        url: source_url,
+        site_name: parsed_article.siteName,
+        published_time: parsed_article.publishedTime,
+        content: parsed_article.content
+    });
 }
 
 app.get('/', (req, res) => {
     if(req.query[config.DOC_URL]){
-        const request = new Request(req.query[config.DOC_URL]);
+        const doc_url = req.query[config.DOC_URL];
+        const request = new Request(doc_url);
         fetch(request).then((response) => {
             if(response.ok) {
                 response.text().then((html_body) => {
                     let doc = new JSDOM(html_body, {
-                        url: req.query[config.DOC_URL]
+                        url: doc_url
                     })
                     let reader = new Readability(doc.window.document);
                     let article = reader.parse();
-                    res.send(response_page(article));
+                    res.send(response_page(article, doc_url));
                 })
             }
         })
@@ -37,6 +53,6 @@ app.get('/', (req, res) => {
     }
 })
 
-app.listen(port, () => {
-  console.log(`Reader is running at ${port}!`)
+app.listen(config.SERVER_PORT, () => {
+  console.log(`Read.LKL is running at ${config.SERVER_PORT}!`);
 })
